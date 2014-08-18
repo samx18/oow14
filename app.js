32bac1961193f69b0e4d357d5968d9b897fbf7b2
Sam <sampat.palani@gmail.com>
2014-08-18 12:12:25 -0600

    Added mongoose & connection to mongoDB

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
 //Configure
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
+//mongoose.connect('mongodb://127.0.0.1/users');
+mongoose.connect('mongodb://127.0.0.1/fifatweetminem1');
+//var peopleSchema = new mongoose.Schema({ name: String});
+mongoose.model('xyz', {name: String});
+//var People = mongoose.model('people', peopleSchema);
+
 
 // Use middletier
 
@@ -12,6 +19,13 @@ app.set("view engine", "ejs");
 // Define routes
 app.get("/", function(req,res){
   res.render("index");
+});
+
+app.get("/tweet", function(req,res){
+
+  mongoose.model('xyz','tweets').findOne(function(err, people){    //create a modles'xyz' and pass in the collection name
+     res.send(people)                                              //Find the first tweet with mongoDB findOne()
+  });
 });
 
 // Start server
